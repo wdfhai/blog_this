9d84930f9747177eb0f2dd28ac816b051428a441
wdfhai <wd.fhai@outlook.com>
2021-03-11 23:01:06 -0600

    completed View componenet

---
+++ public/js/script.js
@@ -0,0 +1,79 @@
+async function signUpHandler(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#signUpName').value;
+    const email = document.querySelector('#signUpEmail').value;
+    const password = document.querySelector('#signUpPassword').value;
+    const password = document.querySelector('#matchPassword').value;
+
+    const response = await fetch(`/api/userRoutes`, {
+        method: 'POST',
+        body: JSON.stringify({
+        name,
+        email,
+        password,
+        }),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to sign up.');
+    }
+}
+
+async function loginHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#loginEmail').value;
+    const password = document.querySelector('#loginPassword').value;
+
+    const response = await fetch(`/api/userRoutes`, {
+        method: 'GET',
+        body: JSON.stringify({
+        email,
+        password,
+        }),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log in.');
+    }   
+}
+
+async function newBlogHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#newBlogTitle').value;
+    const text = document.querySelector('#newBlogText').value;
+
+    const response = await fetch(`/api/blogRoutes`, {
+        method: 'POST',
+        body: JSON.stringify({
+        title,
+        text,
+        }),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to save your blog.');
+    }   
+}
+  
+document.querySelector('#signUpBtn').addEventListener('submit', signUpHandler);
+document.querySelector('#loginBtn').addEventListener('submit', loginHandler);
+document.querySelector('#newBlogBtn').addEventListener('submit', newBlogHandler)
+  
